e410c3543c2f0f26661d96cdf55b8bcbf3933c23
kiarash.avin <kiarash.kiani@avinance-media.com>
2020-04-04 13:51:50 +0430

    init commit

--- Sources/SwiftyUserDefault/SwiftyUserDefault.swift
+++ Sources/SwiftyUserDefault/SwiftyUserDefault.swift
@@ -1,3 +1,24 @@
-struct SwiftyUserDefault {
-    var text = "Hello, World!"
+import Foundation
+
+@propertyWrapper
+public struct UserDefault<T> {
+    var key: String
+    var defaultValue: T
+    
+    init(key: String, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+    
+    public var wrappedValue: T{
+        get{
+            return (UserDefaults.standard.value(forKey: self.key) as? T) ?? self.defaultValue
+        }
+        set{
+            UserDefaults.standard.set(newValue, forKey: self.key)
+        }
+    }
 }
+
+
+
